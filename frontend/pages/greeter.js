5940303ebeb27103f203ddd1e2d7a47fbdfb074d
Frenzoid <frenzoid@protonmail.com>
2022-05-27 03:25:29 +0200

    Added events functionality

--- frontend/pages/greeter.js
+++ frontend/pages/greeter.js
@@ -4,7 +4,7 @@ import { DEPLOYER_ADDRESS } from "../config/constants";
 export default function Greeter({ onTransaction, wrongNetwork, getContract, setOnTransaction, getENSorAdress }) {
 
   const [greeter, setGreeter] = useState(null);
-  const [greeterEvent, setGreeterEvent] = useState([]);
+  const [greeterEvents, setGreeterEvents] = useState([]);
   const newGreet = useRef(null);
 
 
@@ -18,7 +18,7 @@ export default function Greeter({ onTransaction, wrongNetwork, getContract, setO
       await tx.wait();
       setOnTransaction(false);
 
-      greet();
+      loadViewData();
     }
   }
 
@@ -30,11 +30,34 @@ export default function Greeter({ onTransaction, wrongNetwork, getContract, setO
 
   const setGreetEventListener = async () => {
     const contract = getContract();
+
+    // This is a filter ( gets all events, just once )
+    const eventFilter = contract.filters.Greet();
+    const events = await contract.queryFilter(eventFilter);
+
+    let greetEvents = [];
+    await Promise.all(events.map(async event => {
+      const from = await getENSorAdress(event.args[0]);
+      const greeting = event.args[1];
+      greetEvents.push({ from, greeting });
+    }));
+
+    // Newest firsts :)
+    greetEvents.reverse();
+    setGreeterEvents(greetEvents);
+
+
+
+    // This is a listener ( gets new events, live load )
+    /*
     contract.on('Greet', async (address, greeting) => {
       const from = await getENSorAdress(address);
-      const greetEvents = [...greeterEvent, `${from} says: ${greeting}`];
-      setGreeterEvent(greetEvents);
-    });
+      const greetEvents = [...greeterEvent, { from, greeting }];
+
+      // Newest firsts :)
+      greetEvents.reverse();
+      setGreeterEvents(greetEvents);    });
+    */
   }
 
   const loadViewData = async () => {
@@ -48,15 +71,43 @@ export default function Greeter({ onTransaction, wrongNetwork, getContract, setO
     if (!wrongNetwork) loadViewData();
   }, [wrongNetwork]);
 
+  // Components render.
+  const renderBody = () => {
+    return (
+      <div>
+        <h1 className={"text-center mt-2 mb-2"}>{greeter}</h1>
+        <div className={"input-group mb-3"}>
+          <input type="text" ref={newGreet} className={"form-control"} />
+          <div className={"input-group-append"}>
+            <button className={"btn btn-primary"} type="button" onClick={setGreeting}>Say hello to the world!</button>
+          </div>
+        </div>
+        <div>Created by <a href="https://frenzoid.dev" className={"text-primary"}>{DEPLOYER_ADDRESS} - MrFrenzoid</a> </div>
+      </div>
+    );
+  }
+
+
+  const renderLoadingGif = () => {
+    if (onTransaction)
+      return (
+        <div className={"text-center mt-4"} >
+          <h4>Transaction in progress</h4>
+          <img width="50%"
+            src="./loading.gif" alt="Loading..." />
+        </div>
+      );
+  }
+
 
   const renderGreetEventList = () => {
-    if (greeterEvent.length > 0) {
+    if (greeterEvents.length > 0 && !onTransaction) {
       return (
-        <div className={"mt-5"}>
+        <div className={"mt-4"}>
           <h3>Greeting Events: </h3>
           <ul>
-            {greeterEvent.map((event, index) => {
-              return <li key={index}>{event}</li>
+            {greeterEvents.map((event, index) => {
+              return <li key={index}><span className={"text-danger"}>{event.from}</span> says: <i>{event.greeting}</i></li>
             })}
           </ul>
         </div>
@@ -65,37 +116,11 @@ export default function Greeter({ onTransaction, wrongNetwork, getContract, setO
   }
 
 
-  // Components render.
-  const renderLoadingGif = () => {
-    if (onTransaction)
-      return (
-        <div className={"text-center"} >
-          <img width="50%"
-            src="./loading.gif" alt="Loading..." />
-        </div>
-      );
-  }
-
-  const renderBody = () => {
-    return (
-      <div>
-        <h1 className={"text-center mt-2 mb-2"}>{greeter}</h1>
-        <div className={"input-group mb-3"}>
-          <input type="text" ref={newGreet} className={"form-control"} />
-          <div className={"input-group-append"}>
-            <button className={"btn btn-primary"} type="button" onClick={setGreeting}>Set Greeter</button>
-          </div>
-        </div>
-        <div>Created by <code><a href="https://frenzoid.dev">{DEPLOYER_ADDRESS} - MrFrenzoid</a></code> </div>
-        {renderGreetEventList()}
-      </div>
-    );
-  }
-
   return (
     <div>
       {renderBody()}
       {renderLoadingGif()}
+      {renderGreetEventList()}
     </div>
   );
 }
